cfbb69d0d5bc3d78d21cff01f90daffb00d34414
Danny Peck <dannypeck@gmail.com>
2011-09-27 12:22:59 -0400

    Deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ default_run_options[:pty] = true
 set :user, 'dpeck01'
 set :domain, 'symmetricalrecords.com'
 set :application, 'Symmetrical-Records'
+set :bundle_cmd, "/home/dpeck/.gems/bin/bundle"
 
 # the rest should be good
 set :repository,  "git@github.com:dep/#{application}.git"
