ffdab4cf068d9f3f0f6acd63098a3bf499a2f9f4
Danny Peck <dannypeck@gmail.com>
2011-09-27 11:52:20 -0400

    Changes to DB. Deployment stuff.

---
+++ config/deploy.rb
@@ -0,0 +1,25 @@
+default_run_options[:pty] = true
+
+# be sure to change these
+set :user, 'dpeck01'
+set :domain, 'symmetricalrecords.com'
+set :application, 'Symmetrical-Records'
+
+# the rest should be good
+set :repository,  "git@github.com:dep/#{application}.git"
+set :deploy_to, "/home/#{user}/#{domain}"
+set :deploy_via, :remote_cache
+set :scm, 'git'
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
+set :use_sudo, false
+
+server domain, :app, :web
+role :db, domain, :primary => true
+
+namespace :deploy do
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
